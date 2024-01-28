3a7e97d28ee725ad7b0069fd026d6f3e1aef3b48
BeastWalnut <Dieguitoleyva12+github@gmail.com>
2024-01-28 14:50:02 -0700

    test: added directions as string

--- test.lua
+++ test.lua
@@ -1,9 +1,23 @@
 local Tracker = require("api");
 local USER = "BeastWalnut";
 local TARGET = "Beast";
+local DIRECTIONS = {
+	[-4] = "Back",
+	[-3] = "Back Left",
+	[-2] = "Left",
+	[-1] = "Front Left",
+	[0] = "Front",
+	[1] = "Front Right",
+	[2] = "Right",
+	[3] = "Back right",
+	[4] = "Back",
+};
+
+term.clear();
+term.setCursorPos(1, 1);
 
 if type(arg[1]) == "string" then
-	TARGET = arg[1]
+	TARGET = arg[1];
 end
 
 local tracker = Tracker:new(USER);
@@ -20,19 +34,19 @@ if #online_players == 0 then
 else
 	print("Online players:")
 	for _, player in ipairs(online_players) do
-		print(" -" .. player)
+		print(" -" .. player);
 	end
 end
 
 print("Finding: " .. TARGET);
 local position = tracker:relative_find(TARGET);
 if (not position) and (not tracker:is_online(TARGET)) then
-	print("Target: " .. TARGET .. " Is not online.")
+	print("Target: " .. TARGET .. " Is not online.");
 	return;
 end
 
 if not position then
-	print(TARGET .. " Is in another dimension.")
+	print(TARGET .. " Is in another dimension.");
 	return;
 end
 
@@ -41,8 +55,11 @@ print("Y: " .. position.y);
 print("z: " .. position.z);
 
 if position.distance then
-	print("Distance: " .. position.distance);
+	print("Distance: " .. math.ceil(position.distance));
 end
 if position.direction then
-	print("Direction: " .. position.direction .. " degrees")
+	local direction = DIRECTIONS[math.round(position.direction / 45)] or "Error";
+	print("Direction: " .. direction);
+
+	print("Angle: " .. math.floor(position.direction) .. " degrees");
 end
