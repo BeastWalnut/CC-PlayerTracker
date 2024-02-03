d76462e87d704e78d98c944050881a35e7b7d750
BeastWalnut <Dieguitoleyva12+github@gmail.com>
2024-02-02 22:13:04 -0700

    fix: fixed install file and added more descriptive errors

--- install.lua
+++ install.lua
@@ -22,23 +22,30 @@ local function github_get(repo, folder)
 	local files = textutils.unserialiseJSON(response.readAll() or "");
 	response.close();
 	if not files then
-		print("Repo is empty");
-		return;
+		print("Error finding repository: " .. repo);
+		return
 	end
 
 	for _, file_data in ipairs(files) do
 		if file_data.name ~= "install.lua" then
 			local file_response = http.get(file_data.download_url);
+			local path = folder .. file_data.name;
 			if file_response then
-				local file = fs.open(folder .. file_data.name, "w");
+				local file = fs.open(path, "w");
 				if file then
 					file.write(file_response.readAll() or "");
 					file.close();
+				else
+					print("Could not edit file: " .. path);
 				end
 				file_response.close();
+			else
+				print("Could not install: " .. file_data.name);
 			end
 		end
 	end
 end
 
-github_get("BeastWalnut.CC-PlayerTracker", ".tracker/")
+term.clear();
+term.setCursorPos(1, 1);
+github_get("BeastWalnut/CC-PlayerTracker", ".tracker/")
